dbace78ae99a8c22906ce6157e3bc71ef24f06f6
= <rennygorozabel@gmail.com>
2020-12-01 00:21:13 -0500

    Playing with material iu

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import {Grid, Container, Box, Button, Typography} from '@material-ui/core';
+import Avatar from '@material-ui/core/Avatar';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles({
+  avatar:{
+    color: 'green'
+  },
+  p1:{
+    padding:8
+  },
+  aLeft:{
+    textAlign:"left"
+  }
+})
 
 function App() {
+  const classes = useStyles();
   return (
     <div className="App">
+      <Container>
+      <Grid container>
+        <Grid item xs={1}>
+            <Box p={1}>
+              <Avatar className={classes.avatar} alt="profile" src="/static/avatar.jpg"/>
+            </Box>
+        </Grid>
+        <Grid item xs={3}>
+          <Box component="p" className={classes.aLeft}>
+            <Typography variant="subtitle2" >Usuario 1</Typography>
+          </Box>
+        </Grid>
+        <Grid item xs={2} sm={4}></Grid>
+        <Grid item xs={4} sm={2}>
+          <Box p={1}>
+          <Button variant="outlined" color="default">
+                B
+            </Button>
+          </Box>
+        </Grid>
+      </Grid>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
+      </Container>
     </div>
   );
 }
